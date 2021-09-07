54b362961e06ba4f2cf7edb15b3413c0cdce46d4
Maryust <masyanya1101@yandex.ru>
2021-09-07 13:22:08 +0300

    Homework1.1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int account = 2_000_000_000;
+        int moneytransfer = 500_000_000;
+        int total = account + moneytransfer;
+        System.out.println(total);
+    }
+}
